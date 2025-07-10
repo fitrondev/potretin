32e565c58d6090b619e12cea153055bc28762479
fitronansori <fitronansori7@gmail.com>
2025-07-10 14:23:33 +0800

    feat: update Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,10 @@
 import About from "@/components/layouts/About";
+import Contact from "@/components/layouts/Contact";
+import Faq from "@/components/layouts/Faq/Index";
 import Hero from "@/components/layouts/Hero";
 import Portofolio from "@/components/layouts/Portofolio";
+import Pricing from "@/components/layouts/Pricing";
+import Testimonials from "@/components/layouts/Testimonials";
 
 const Home = () => {
   return (
@@ -8,6 +12,10 @@ const Home = () => {
       <Hero />
       <About />
       <Portofolio />
+      <Testimonials />
+      <Pricing />
+      <Contact />
+      <Faq />
     </main>
   );
 };
